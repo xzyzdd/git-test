31d2048012f7d64dd88c166cf64df77805210aaf
zxx <2337564735@qq.com>
2022-08-22 17:52:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zxx
+ * @create 2022-08-22-17:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
